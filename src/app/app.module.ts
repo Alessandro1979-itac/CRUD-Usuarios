456678fee28095165412cb8d7b8cd45e5a8561bc
Alessandro <sandromuniz27@hotmail.com>
2022-04-01 19:25:54 -0400

    first commit - crud implementado

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { UserFormComponent } from './pages/users/user-form/user-form.component';
+import { UsersListComponent } from './pages/users/users-list/users-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    UserFormComponent,
+    UsersListComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
